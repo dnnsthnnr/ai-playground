425cec581dec11eaba13132eb5098084760597fe
Dennis Thanner <dennis-thanner@web.de>
2017-09-14 00:23:04 +0200

    added safari demo warning

--- ai-playground/src/app/sketch-classification/components/sketch-draw/sketch-draw.component.ts
+++ ai-playground/src/app/sketch-classification/components/sketch-draw/sketch-draw.component.ts
@@ -20,6 +20,8 @@ export class SketchDrawComponent implements AfterViewInit {
 
   private isDrawing = false;
 
+  private waringDisplayed = true;
+
   constructor(public modalSvc: NgbModal, public modelSvc: SketchClassificationModelService) {
   }
 
