5061e41b92f6dbe9f18662438e9789bd5b2c36c7
Dennis Thanner <dennis-thanner@web.de>
2017-10-15 14:18:04 +0200

    introduced different cli apps for dev and prod excluded ga from dev

--- python/utils.py
+++ python/utils.py
@@ -11,8 +11,11 @@ import wget as wget
 from PIL import Image as pil_image
 from keras import backend as K
 import tensorflow as tf
+from keras.engine import Layer
 
 from python import dump_ckpt_vars
+import sys
+from datetime import datetime
 
 DATA_DIR = "../data"
 MODEL_DIR = "../model"
@@ -111,15 +114,18 @@ def augment_image(img):
 
 
 def image_to_array(img, normalize=True):
-    img = np.asarray(img)
+    img = np.asarray(img).astype(K.floatx())
     if normalize:
         img = img / 255
     return img
 
 
 def maybe_transform_image_matrix_to_3_channels(m):
-    if m.shape[2] == 1:
+    if len(m.shape) < 3:
+        # m = m.reshape((m.shape[0], m.shape[1], 1))
+        # if m.shape[2] == 1:
         m = np.stack((m,) * 3, axis=-1)
+
     return m
 
 
@@ -137,8 +143,71 @@ def export_keras_for_deeplearn(name):
     dump_ckpt_vars.main(ckpt_path, "../ai-playground/src/assets/deeplearn/" + name)
 
 
-def progress_bar(current, total, width):
+def pad_image_to_square(img, mode=None, fill="white"):
+    w, h = img.size
+    max_side = max(w, h)
+    if mode is None:
+        mode = img.mode
+    pad_image = pil_image.new(mode, (max_side, max_side), fill)
+    # print(w, h, (int((max_side - w) / 2), int((max_side - h) / 2)))
+    pad_image.paste(img, (int((max_side - w) / 2), int((max_side - h) / 2)))
+    # pad_image.show()
+    return pad_image
+
+
+def array_to_img(arr, denormalize=True):
+    if denormalize:
+        arr = arr * 255
+    arr = arr.astype('uint8')
+
+    if arr.shape[2] == 1:
+        arr = arr.reshape((arr.shape[0], arr.shape[1]))
+
+    return pil_image.fromarray(arr)
+
+
+def progress_bar(current, total, width=None):
     print("\r{0}%".format(round(current * 100 / total, 2)), end="")
     if current == total:
         # get new line
         print()
+
+
+class Denormalize(Layer):
+    '''
+    Custom layer to denormalize the final Convolution layer activations (tanh)
+    Since tanh scales the output to the range (-1, 1), we add 1 to bring it to the
+    range (0, 2). We then multiply it by 127.5 to scale the values to the range (0, 255)
+    '''
+
+    def __init__(self, **kwargs):
+        super(Denormalize, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        pass
+
+    def call(self, x, mask=None):
+        '''
+        Scales the tanh output activations from previous layer (-1, 1) to the
+        range (0, 255)
+        '''
+
+        return (x + 1) * 127.5
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+class ImageNormalize(Layer):
+    def __init__(self, **kwargs):
+        super(ImageNormalize, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        pass
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+    def call(self, x, mask=None):
+        # x = (x - 127.5)/ 127.5
+        return x / 255.
